4614a205af7925334e2f7815391f9ad3f4ae5ec3
Jack Liu <liu.zhifeng09@yahoo.com>
2020-07-11 20:37:26 -0400

    deploy

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+<script>
+    $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $("#mycarousel").carousel({
+            interval: 2000
+        });
+
+        $("#carouselButton").click(function () {
+            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                $("#mycarousel").carousel('pause');
+                $("#carouselButton").children("span").removeClass('fa-pause');
+                $("#carouselButton").children("span").addClass('fa-play');
+            } else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+                $("#mycarousel").carousel('cycle');
+                $("#carouselButton").children("span").removeClass('fa-play');
+                $("#carouselButton").children("span").addClass('fa-pause');
+            }
+        });
+
+        $("#reserve").click(function () {
+            $("#reserveModal").modal();
+        });
+
+        $("#login").click(function () {
+            $("#loginModal").modal();
+        });
+
+    });
+</script>
